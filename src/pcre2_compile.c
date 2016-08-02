b53574bf91a1304f1b2476c0ec731356cd1fe773
Matthew Vernon <matthew@debian.org>
2016-08-02 18:56:54 +0100

    Revert "Apply upstream revision 489 (Closes: #815920)"
    This reverts commit 6e2f9220620fa8f0f3a3ef62d940b9fcd4cedea3.
    
    We do this as the new upstream version has fixed the bug this fixes.

--- src/pcre2_compile.c
+++ src/pcre2_compile.c
@@ -5899,22 +5899,10 @@ for (;; ptr++)
               goto FAILED;
               }
             cb->had_accept = TRUE;
-            
-            /* In the first pass, just accumulate the length required;
-            otherwise hitting (*ACCEPT) inside many nested parentheses can
-            cause workspace overflow. */
-              
             for (oc = cb->open_caps; oc != NULL; oc = oc->next)
               {
-              if (lengthptr != NULL)
-                {
-                *lengthptr += CU2BYTES(1) + IMM2_SIZE; 
-                }
-              else
-                {       
-                *code++ = OP_CLOSE;
-                PUT2INC(code, 0, oc->number);
-                } 
+              *code++ = OP_CLOSE;
+              PUT2INC(code, 0, oc->number);
               }
             setverb = *code++ =
               (cb->assert_depth > 0)? OP_ASSERT_ACCEPT : OP_ACCEPT;
